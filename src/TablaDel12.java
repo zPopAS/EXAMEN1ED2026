ba689d61ceaf15b555872318b79fe75141bfb15c
JuanPop <juanpop@gmail.com>
2026-01-12 18:57:50 +0100

    JuanPop añade println sobre que se ha inprimido la tabla del 12 correctamente.

--- src/TablaDel12.java
+++ src/TablaDel12.java
@@ -5,5 +5,6 @@ public class TablaDel12{
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
+        System.out.println("Se ha imprimido por pantalla la tabla del 12.");
     }
 }
