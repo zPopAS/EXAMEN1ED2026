97c114fe5ee9eee1ca13a4ff1942f17ce44a25c7
AbelPop <abelpop@gmail.com>
2026-01-12 18:52:23 +0100

    Primer commit

---
+++ src/TablaDel12.java
@@ -0,0 +1,9 @@
+public class TablaDel12{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
