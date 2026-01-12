940fedec71047f2e9b19bfe669d1057e81fbb6f5
LorenaPop <lorenapop@gmail.com>
2026-01-12 19:05:05 +0100

    LorenaPop adjunta un nuevo println sobre la siguiente tabla del 13.

--- src/TablaDel12.java
+++ src/TablaDel12.java
@@ -6,5 +6,6 @@ public class TablaDel12{
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
         System.out.println("Se ha imprimido por pantalla la tabla del 12.");
+        System.out.println("En el siguiente fichero se estará la tabla del 13.");
     }
 }
